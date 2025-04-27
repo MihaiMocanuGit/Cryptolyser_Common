854303ff1894cc4d7e686ce9cb53c1e4b76998b5
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-04-27 15:46:21 +0300

    Clangd Bug | Workaround for a known false positive warning.

--- Cryptolyser_Common/connection_data_types.h
+++ Cryptolyser_Common/connection_data_types.h
@@ -1,22 +1,24 @@
-#ifndef CRYPTOLYSER_CONNECTION_DATA_TYPES_H
-#define CRYPTOLYSER_CONNECTION_DATA_TYPES_H
+#pragma once
 
 #include <stdint.h>
 
 #define CONNECTION_DATA_MAX_SIZE (1024 - 3 * sizeof(uint32_t))
+#define PACKET_KEY_BYTE_SIZE 16
+#define AES_BLOCK_BYTE_SIZE 16
+#define PACKET_RESPONSE_DATA_SIZE AES_BLOCK_BYTE_SIZE
 
-#pragma pack(1)
+static_assert(true); // dummy declaration, ends the preamble https://stackoverflow.com/a/72907523
+                     // (Known bug in clangd causes "1. Unterminated '#pragma pack (push, ...)' at
+                     // end of file [-Wpragma-pack]" warning.
+#pragma pack(push, 1)
+
 struct connection_packet_t
 {
     uint32_t packet_id;
     uint32_t data_length;
     uint8_t byte_data[CONNECTION_DATA_MAX_SIZE];
 };
-#pragma pack(0)
 
-#define PACKET_KEY_BYTE_SIZE 16
-#define AES_BLOCK_BYTE_SIZE 16
-#pragma pack(1)
 struct connection_key_packet_t
 {
     uint32_t packet_id;
@@ -24,10 +26,7 @@ struct connection_key_packet_t
     uint32_t data_length;
     uint8_t byte_data[CONNECTION_DATA_MAX_SIZE];
 };
-#pragma pack(0)
 
-#define PACKET_RESPONSE_DATA_SIZE AES_BLOCK_BYTE_SIZE
-#pragma pack(1)
 struct connection_response_t
 {
     uint32_t packet_id;
@@ -37,6 +36,5 @@ struct connection_response_t
     uint64_t outbound_t1;
     uint64_t outbound_t2;
 };
-#pragma pack(0)
 
-#endif // CRYPTOLYSER_CONNECTION_DATA_TYPES_H
+#pragma pack(pop)
